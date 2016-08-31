b3fd7d7a41ab6abd9bf3b69c7560b748a81aa9dd
Michael A. Teti <mtetifau@gmail.com>
2016-08-31 11:28:49 -0400

    Update choosedata.m

--- choosedata.m
+++ choosedata.m
@@ -1,3 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%--------------------------------------------------------------------------
+%
+%                         Michael A. Teti
+%
+%            Machine Perception and Cognitive Robotics Lab
+%
+%            Center for Complex Systems and Brain Sciences
+%
+%                    Florida Atlantic University
+%
+%--------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%--------------------------------------------------------------------------
+%
+% This program is an attempt to detect bird presence in highly noisy 
+% and heterogenous images taken by a game camera every three minutes from 
+% 6am to 2pm for one week. This particular program is used to select the
+% training images for a convolutional neural network.
+%
+%--------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc;
+clear all;
+close all; 
+
 cd('/media/mpcr/5EBA074CBA071FDF/bird');
 
 patches=[];
@@ -18,40 +45,48 @@ for j=1:length(files); % loop through all images in folder
             patches=[patches im_patch]; %compile image vectors
             num_patches=num_patches+1;
         end
+    elseif x<33
+        continue 
+    elseif x>(size(im, 2)-33)
+        continue
     else 
         continue
     end 
 end 
 
-if num_patches~=size(patches, 2)
-    disp('Patches not loaded correctly. Stop now');
-end   
+
 labels=ones(size(patches, 2), 1);
 m=length(labels);
 pause;
 
 cd('/media/mpcr/5EBA074CBA071FDF/M4_3');
-while j<400
-    files=dir('*.JPG'); % list all .jpg images in your path 
-    for j=1:length(files); % loop through all images in folder
-        im=im2double(imread(files(j).name)); % read each image 
-        figure(1)
-        imagesc(im)
-        fig=gcf();
-        [x,y]=getpts(fig);
-        x=floor(x); y=floor(y);
-        if y<1500
-            for r=1:numel(x)
-                im_patch=im(y(r)-32:y(r)+32, x(r)-32:x(r)+32, :);
-                im_patch=im_patch(:);
-                labels=[labels; 0];
-            end
-        else
-            continue 
+
+files=dir('*.JPG'); % list all .jpg images in your path 
+for j=1:m; % loop through all images in folder
+    im=im2double(imread(files(j).name)); % read each image 
+    figure(1)
+    imagesc(im)
+    fig=gcf();
+    [x,y]=getpts(fig);
+    x=floor(x); y=floor(y);
+    if y<1500
+        for r=1:numel(x)
+            im_patch=im(y(r)-32:y(r)+32, x(r)-32:x(r)+32, :);
+            im_patch=im_patch(:);
+            labels=[labels; 0];
         end
-        patches=[patches im_patch]; %compile image vectors
+    elseif x<33
+        continue 
+    elseif x>(size(im, 2)-33)
+        continue
+    else
+        continue 
     end
-end 
+    patches=[patches im_patch]; %compile image vectors
+end
+ 
+
+[patches , labels]=add_examples(patches, labels);
 
 cd('/home/mpcr/Desktop/lila_birds');
 save('patch_data.mat', 'patches');
