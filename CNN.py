eea33dbda23a814d10007fd4841e8ffc1e57317f
Michael A. Teti <mtetifau@gmail.com>
2016-09-29 16:44:06 -0400

    99.1 % accuracy

--- CNN.py
+++ CNN.py
@@ -1,5 +1,6 @@
 import scipy
 from scipy.io import loadmat
+from scipy.io import savemat
 import scipy.misc
 import numpy as np
 import os 
@@ -7,107 +8,95 @@ import sys
 sys.path.append('/home/mpcr/newproject/venv/lib/python2.7/site-packages')
 import tensorflow as tf
 
-# load data and ground truth
-a=loadmat('lab.mat')
-b=loadmat('datapatches.mat')
-labels=a['labels']
-data=b['patches']
-patch_channels=3
-patch_size=np.ma.size(data, 1)
-output=1
-batch=100
-c1=75
-c2=150
-c3=200
-c4=300
-h1=2500
-h2=2500
+# load data and labels
+a=loadmat('whitened_2ch.mat') 
+data=a['whitened_2ch'] # whitened data
+b=loadmat('labels.mat')
+labels=b['labels'] # labels
 
-sess=tf.InteractiveSession() # begin tensorflow session
+# Create training dataset and testing dataset
+perm=np.random.permutation(np.ma.size(data, 0))
+data=data[perm, :]
+labels=labels[perm]
+train_data=data[:(np.ma.size(data, 0)-np.ma.size(data, 0)*.1), :]
+test_data=data[(np.ma.size(data, 0)-np.ma.size(data, 0)*.1):, :]
+train_labels=labels[:(np.ma.size(data, 0)-np.ma.size(data, 0)*.1)]
+test_labels=labels[(np.ma.size(data, 0)-np.ma.size(data, 0)*.1):]
 
-x = tf.placeholder(tf.float32, shape=[None, patch_size])
-y = tf.placeholder(tf.float32, shape=[None, output])
-keep_prob = tf.placeholder(tf.float32)
+patch_size=np.ma.size(data, 1) # 130x130x2 input 
+output=1 # one output node
+h1=np.round(25*60*.5, decimals=0).astype(int) # num. of fully connected layer nodes
 
-def training_set(X, y):
-	td=np.empty([1, patch_size])
-	labels=np.zeros([batch, 1])
-	for i in range(batch):
-		rand_ex=np.random.permutation(np.ma.size(X, 0))
-		r=rand_ex[0:batch]
-		train_data=X[r[i], :]
-		train_data=train_data.flatten()
-		train_data=train_data[np.newaxis, :]
-		td=np.concatenate((td, train_data), axis=0)
-		label=y[r[i]]
-		labels[i]=label
-	td=td[1:, :]
-	if np.ma.size(td, 0)!=batch:
+# Create random training set each iteration of gradient descent 
+def training_set(X, y, batch):
+	rand_ex=np.round((np.random.rand(batch, 1)*(np.ma.size(X, 0)-1)), 
+		decimals=0).astype(int)
+	td=X[rand_ex.flatten(), :]
+	labels=y[rand_ex.flatten()]
+	if np.ma.size(td, 0)!=batch and np.ma.size(td, 1)!=patch_size:
 		print 'Data not loaded correctly. Goodbye'
 		sys.exit()
 	return td, labels
 
-# create small, random weights
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+sess=tf.InteractiveSession() # begin tensorflow session
+x = tf.placeholder(tf.float32, shape=[None, patch_size]) # Input layer 
+y = tf.placeholder(tf.float32, shape=[None, output]) # labels
+keep_prob = tf.placeholder(tf.float32) # dropout probability for fc layer
 
-# create biases
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+# Network
+out=tf.nn.conv2d(tf.reshape(x, [-1, 130, 130, 2]),
+	tf.Variable(tf.truncated_normal([8, 8, 2, 15],
+	stddev=0.1)), strides=[1, 2, 2, 1], padding='SAME')
+out=tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[15])))
+out=tf.nn.max_pool(out, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+	padding='SAME')
+out=tf.nn.conv2d(out, tf.Variable(tf.truncated_normal([4, 4, 15, 25],
+	stddev=0.1)), strides=[1, 1, 1, 1], padding='SAME')
+out=tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[25])))
+out=tf.nn.max_pool(out, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+	padding='SAME')
+out=tf.nn.conv2d(out, tf.Variable(tf.truncated_normal([3, 3, 25, 40], 
+	stddev=0.1)), strides=[1, 1, 1, 1], padding='SAME')
+out=tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[40])))
+out=tf.nn.max_pool(out, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], 
+	padding='SAME')
+out=tf.nn.conv2d(out, tf.Variable(tf.truncated_normal([3, 3, 40, 60],
+	stddev=0.1)), strides=[1, 1, 1, 1], padding='SAME')
+out=tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[60])))
+out=tf.nn.max_pool(out, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], 
+	padding='SAME')
+out=tf.matmul(tf.reshape(out, [-1, 25*60]), tf.Variable(tf.truncated_normal
+	([25*60, h1], stddev=0.1)))
+out=tf.nn.dropout(tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[h1]))), 
+	keep_prob)
+out=tf.matmul(out, tf.Variable(tf.truncated_normal([h1, h1], stddev=0.1)))
+out=tf.nn.dropout(tf.nn.relu(out+tf.Variable(tf.constant(0.1, shape=[h1]))),
+	keep_prob)
+out=tf.matmul(out, tf.Variable(tf.truncated_normal([h1, output], stddev=0.1)))
+out=tf.sigmoid(out+tf.Variable(tf.constant(0.1, shape=[output])))
 
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+# Mean squared error cost function
+cost=tf.reduce_sum((y-out)**2, reduction_indices=0)
 
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-X=tf.reshape(x, [-1, 65, 65, patch_channels])
-w1=weight_variable([2, 2, patch_channels, c1])
-bias1=bias_variable([c1])
-act1=max_pool_2x2(tf.nn.relu(conv2d(X, w1)+bias1))
-
-w2=weight_variable([2, 2, c1, c2])
-bias2=bias_variable([c2])
-act2=max_pool_2x2(tf.nn.relu(conv2d(act1, w2)+bias2))
-
-w3=weight_variable([2, 2, c2, c3])
-bias3=bias_variable([c3])
-act3=max_pool_2x2(tf.nn.relu(conv2d(act2, w3)+bias3))
-
-w4=weight_variable([2, 2, c3, c4])
-bias4=bias_variable([c4])
-act4=max_pool_2x2(tf.nn.relu(conv2d(act3, w4)+bias4))
-act4flat=tf.reshape(act4, [-1, 25*c4])
-
-theta1=weight_variable([25*c4, h1])
-bias1=bias_variable([h1])
-activation2=tf.nn.relu(tf.matmul(act4flat, theta1) + bias1)
-activation2 = tf.nn.dropout(activation2, keep_prob)
-
-theta2=weight_variable([h1, output])
-bias2=bias_variable([output])
-out=tf.nn.softmax(tf.matmul(activation2, theta2) + bias2)
-
-cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(out), reduction_indices=[1]))
-
+# Gradient Descent Optimizer and calculate accuracy for each output
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cost)
-correct_prediction = tf.equal(tf.argmax(out, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.round(out), y), tf.float32))
 
-sess.run(tf.initialize_all_variables())
+saver=tf.train.Saver() # Saves the model parameters after training
 
-training_iters=10000
+sess.run(tf.initialize_all_variables()) # initialize all variables
 
-# training
-for i in range(training_iters):
-  train_data, train_labels=training_set(data, labels)
-  if i%100 == 0:
+# Run and train graph
+for i in range(15000): # training iterations
+  training_data, training_labels=training_set(train_data, train_labels, 90)
+  if i%1 == 0:
     train_accuracy = accuracy.eval(feed_dict={
-        x:train_data, y:train_labels, keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-    #tf.Print(y_, train_labels)
-  train_step.run(feed_dict={x: train_data, y: train_labels, keep_prob: 0.75})
-
+        x:training_data, y:training_labels, keep_prob: 1.0})
+    print("step %d, training accuracy: %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: training_data, y: training_labels, keep_prob: 0.5})
+  test_accuracy=accuracy.eval(feed_dict={x:test_data, y:test_labels, keep_prob:1.0})
+  if test_accuracy > .993: # test accuracy on 10% of data
+    print 'testing accuracy: %.4f' % (test_accuracy)
+    # Save modified variables
+    save_variables=saver.save(sess, '/home/mpcr/Desktop/lila_birds/bird.ckpt')
+    sys.exit() # exit and save parameters if >= 99.3% accuracy reached
