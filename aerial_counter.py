36d3d3e0fe7fde2c3ce65365d91dceb5d5da70cf
Michael A. Teti <mtetifau@gmail.com>
2017-01-26 13:32:00 -0500

    cleaned up some stuff

--- aerial_counter.py
+++ aerial_counter.py
@@ -9,7 +9,9 @@ from scipy.misc import imshow
 
 def get_contours(image, to_hsv=None, thresh=None, smooth=None, edge=None):
   """
-  A function to get contours from a BGR image using opencv.
+  A function to get contours from a BGR image using opencv with optional thresholding
+  and smoothing first.
+
   Args:
        to_hsv: if this value is set to True, the BGR image will be transformed
                to hsv colorspace. Default leaves it as BGR colorspace.
@@ -26,16 +28,20 @@ def get_contours(image, to_hsv=None, thresh=None, smooth=None, edge=None):
                image. It should consist of a 2-vector signifying the lower bound 
                and upper bound. 
   """
-
   if to_hsv is True:
     image=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   
   if thresh is not None:
-    mask=cv2.inRange(image, thresh[0, :].astype(int), thresh[1, :].astype(int))
+    if np.shape(thresh)!=(2, 3):
+      raise TypeError('thresh must be a 2x3 array.')
+    else:
+      mask=cv2.inRange(image, thresh[0, :].astype(int), thresh[1, :].astype(int))
 
   if smooth is not None:
     if 'mask' in locals():
       mask=cv2.bilateralFilter(np.uint8(mask), smooth[0], smooth[1], smooth[2])
+    else:
+      mask=cv2.bilateralFilter(np.uint8(image), smooth[0], smooth[1], smooth[2])
   
   if edge is not None:
     if 'mask' in locals():
@@ -47,17 +53,18 @@ def get_contours(image, to_hsv=None, thresh=None, smooth=None, edge=None):
   return cnts[0] if imutils.is_cv2() else cnts[1]
     
 
+
 def contour_imcrop(im, contour, patch_size):
   """Creates a bounding box around contour and crops im around contour according
      to patch_size. """
-  
-  x, y, w, h = cv2.boundingRect(contour) # find bounding rectangle
-  add_ht=patch_size-h # compare bounding rect. ht. to desired patch ht.
-  add_wd=patch_size-w # compare bounding rect. width to desired patch width
-  im_pad=np.pad(im, ((patch_size/2, patch_size/2), (patch_size/2, 
-  			  patch_size/2), (0, 0)), 'edge') # pad original image
+  x, y, w, h = cv2.boundingRect(contour) 
+  add_ht=patch_size-h
+  add_wd=patch_size-w 
+  assert((add_ht, add_wd)>=(0, 0)), 'added height: %d, added width: %d'%(add_ht, add_wd)
+  im_pad=np.pad(im, ((patch_size/2, patch_size/2), (patch_size/2, patch_size/2), (0, 0)), 'edge')
   im_crop=im_pad[(y+patch_size/2)-add_ht/2:(y+patch_size/2)+h+add_ht/2,
-                 (x+patch_size/2)-add_wd/2:(x+patch_size/2)+w+add_wd/2, :]
+                 			(x+patch_size/2)-add_wd/2:(x+patch_size/2)+w+add_wd/2, :]
+
   return x, y, w, h, im_crop
 
   
@@ -67,7 +74,6 @@ def doublewrap(function):
   A decorator decorator, allowing to use the decorator to be used without 
   parentheses if not arguments are provided. All arguments must be optional.
   """
-  
   @functools.wraps(function)
   def decorator(*args, **kwargs):
     if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
@@ -86,8 +92,7 @@ def define_scope(function, scope=None, *args, **kwargs):
   The operations added by the function live within a tf.variable_scope(). If
   this decorator is used with arguments, they will be forwarded to the
   variable scope. The scope name defaults to the name of the wrapped
-  function. This function was written by Danijar Hafner and can be found at 
-  https://gist.github.com/danijar.
+  function.
   """
   attribute = '_cache_' + function.__name__
   name = scope or function.__name__
@@ -101,9 +106,20 @@ def define_scope(function, scope=None, *args, **kwargs):
   return decorator
 
 
-
 class NN(object):
-  ''' network ''' 
+  ''' A convolutional neural network.
+      
+      Parameters:
+                 x: input data in the form batch size x features
+                 y: vector with the length batch size containing to the 
+                    correct prediction for the corresponding image.
+           num_out: the number of output nodes for the network.
+                ps: Assumes the width and height of the input image are the
+                    same. The width and height of each patch in x.
+          batch_sz: the number of patches to use in each training iteration.
+   feature_scaling: Whether or not to normalize the data before training. 
+                    Defaults to None, in which no normalization will be
+                    performed. ''' 
 
   def __init__(self, x, y, num_out, ps, batch_sz, feature_scaling=None):
     self.batch_sz=batch_sz # batch size
@@ -132,8 +148,7 @@ class NN(object):
   def prediction(self):
     self.x_=tf.reshape(self.feature_scaling, [-1, self.ps, self.ps, 3])
     slim=tf.contrib.slim
-    net=slim.repeat(self.x_, 3, slim.conv2d, 100, 
-		    [5, 5], padding='VALID', activation_fn=tf.nn.relu)
+    net=slim.repeat(self.x_, 3, slim.conv2d, 100, [5, 5], padding='VALID')
     net=tf.nn.max_pool(net, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
     net=slim.dropout(slim.fully_connected(slim.flatten(net), 1100), keep_prob=0.5)
     net=slim.dropout(slim.fully_connected(net, 1100), keep_prob=0.5)
@@ -159,24 +174,27 @@ class NN(object):
 def main():
 
   # initialize some stuff
-  e_plot=np.zeros([1, ]) # vector to store error values 
-  batch_sz=5 # training batch size to input to the network
-  patch_sz=55 # size of the patch to extract from the image w/contour at center
-  desired_sz=75 # dimension to resize patch to if desired
-  data=np.zeros([1, desired_sz**2*3]) # create a matrix to put the 55x55 input images
-  labels=np.zeros([1, ]) # create numpy array for labels
+  i=1
+  e_plot=np.zeros([1, ]) 
+  batch_sz=5
+  patch_sz=100 
+  num_out_nodes=1 
+
+  data=np.zeros([1, patch_sz**2*3])
+  labels=np.zeros([1, ]) 
+  
   data_holder=tf.placeholder(tf.float32, shape=[None, data.shape[1]])
   label_holder=tf.placeholder(tf.float32, shape=[None, ])
-
-  num_out_nodes=1 # number of output nodes
-  if num_out_nodes==1:
-    num_classes=2
-  else:
-    num_classes=num_out_nodes
-
-  nn=NN(data_holder, label_holder, num_out_nodes, desired_sz, batch_sz, feature_scaling=True)
-  sess=tf.Session() # tensorflow session
-  sess.run(tf.global_variables_initializer()) # initialize variables
+ 
+  nn=NN(data_holder, 
+        label_holder, 
+        num_out_nodes, 
+        patch_sz, 
+        batch_sz, 
+        feature_scaling=None)
+    
+  sess=tf.Session() 
+  sess.run(tf.global_variables_initializer()) 
 
   im=cv2.imread('im.JPG') # read the image - going to have to loop through them all
 
@@ -186,30 +204,24 @@ def main():
                     smooth=np.array([9, 95, 95]))
   
 
-  # loop through each contour
-  for c in cnts:
-    #M = cv2.moments(c)
-    if cv2.contourArea(c)>1200:
+  for c in cnts: # loop through each contour
+    if cv2.contourArea(c)>1200: 
 
       x, y, w, h, im_crop=contour_imcrop(im, c, patch_sz)
+      im_crop=cv2.resize(im_crop, (patch_sz, patch_sz)) 
+      data=np.concatenate((data, im_crop.reshape([1, patch_sz**2*3])), axis=0)
 
-      im_crop=cv2.resize(im_crop, (desired_sz, desired_sz)) # resize the cropped image
- 
-      # vectorize and add to data array
-      data=np.concatenate((data, im_crop.reshape([1, im_crop.size])), axis=0)
-
-      imshow(im_crop) # show the image 
+      imshow(im_crop)
 
       # take user input to create labels for the patch shown
-      label=raw_input('0: no bird, 1: bird, number of classes: see whole map')
+      label=raw_input('0: no bird, 1: bird, 2: see whole map')
       try:
         label=int(label)
-      except ValueError: # check for ValueError in case of wrong input
+      except ValueError: 
         imshow(im_crop)
         label=int(raw_input('Please enter only an integer'))
-
       im_copy=im.copy()
-      if label==num_classes:
+      if label==2:
         while(1):
           cv2.rectangle(im_copy, (x, y), (x+w, y+h), (0, 0, 255), 4)
           cv2.namedWindow('rect contour', cv2.WINDOW_NORMAL)
@@ -218,17 +230,25 @@ def main():
             cv2.destroyAllWindows()
             break
         label=int(raw_input('0: no bird, 1: bird'))
+      assert(label in [0, 1, 2]), 'input must be 0, 1, or 2.'
       labels=np.concatenate((labels, np.array([label, ])), axis=0)
+      print np.shape(data)
+      print labels
 
       # send labels/data to network when data/label input equals desired batch size
       if data.shape[0]%(batch_sz+1)==0:   
         e=sess.run(nn.error, {data_holder: data[1:, :], label_holder: labels[1:, ]})
         e_plot=np.concatenate((e_plot, np.array([e, ])), axis=0)
-        print('Training Error: %.2f'%(e))
+        print("""
+              ===================================
+              Iteration: %d, Training Error: %.2f
+              ===================================
+              """%(i, e))
+
         sess.run(nn.optimize, {data_holder: data[1:, :], label_holder: labels[1:, ]})
-        data=np.zeros([1, desired_sz**2*3]) # empty the data array for next iter
+        data=np.zeros([1, patch_sz**2*3]) # empty the data array for next iter
         labels=np.zeros([1, ]) # empty the label array for next iter
+        i+=1
 
 if __name__ == '__main__':
   main()
-
